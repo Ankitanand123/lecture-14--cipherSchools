94b57ac9e42dd3a27237f25a2e3b1832996fb6ad
Ankitanand123 <117056428+Ankitanand123@users.noreply.github.com>
2024-06-23 14:02:12 +0530

    Create lecture14.cpp

---
+++ lecture14.cpp
@@ -0,0 +1,13 @@
+#include<iostream> //header file 
+using namespace std;
+
+
+void printStuff() //there is no parameters 
+{
+    cout<<"The first Function";
+}
+
+int main(){
+    printStuff();
+    return 0;
+}
